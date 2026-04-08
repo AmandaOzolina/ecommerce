4d1c58a8fd08efa7b49340b7a0b1252fc426e391
Amanda Ozolina <92865615+AmandaOzolina@users.noreply.github.com>
2026-04-08 19:49:15 +0300

    Update server.js

--- warehouse/server.js
+++ warehouse/server.js
@@ -32,5 +32,25 @@ app.post('/products', (req, res) => {
   res.send('Added');
 });
 
+app.put('/products/:id', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(FILE));
+  const index = data.findIndex(p => p.id == req.params.id);
+
+  if (index === -1) return res.status(404).send('Not found');
+
+  data[index] = { ...data[index], ...req.body };
+
+  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+  res.send('Updated');
+});
+
+app.delete('/products/:id', (req, res) => {
+  let data = JSON.parse(fs.readFileSync(FILE));
+  data = data.filter(p => p.id != req.params.id);
+
+  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+  res.send('Deleted');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Warehouse running'));
